103b4480233e3bdf9ea024f7801cad5bf5ef7081
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-06-24 15:56:16 +0500

    fix: Comment out unused backend URL and ensure correct export for active backend URL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
- export const backendUrl = "http://localhost:5001";
+//  export const backendUrl = "http://localhost:5001";
 // export const backendUrl = "https://8rgvpdmw-5000.euw.devtunnels.ms";
 // export const backendUrl = "https://axiom-backend-two.vercel.app";
-//export const backendUrl = "https://axiom-backend-delta.vercel.app";
+export const backendUrl = "https://axiom-backend-delta.vercel.app";
 
 // export const backendUrl = "https://dx8ppwrc-5000.inc1.devtunnels.ms";
 // export const backendUrl = "https://dx8ppwrc-5000.inc1.devtunnels.ms";
